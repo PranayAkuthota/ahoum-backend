daef081ead5fc513262308ac46d449c691710683
PRANAY KUMAR KAUTHOTA <pranaykumarakuthota@MacBook-Air.local>
2026-05-31 14:40:24 +0530

    Add home endpoint

--- core/urls.py
+++ core/urls.py
@@ -1,50 +1,51 @@
 """
 URL configuration for core project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+The `urlpatterns` list routes URLs to views.
 """
+
 from django.contrib import admin
-from django.urls import path
 from django.urls import path, include
+from django.http import JsonResponse
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
-urlpatterns = [
-    path('admin/', admin.site.urls),
 
+
+def home(request):
+    return JsonResponse({
+        "message": "Ahoum Backend Assignment API",
+        "docs": "/api/docs/"
+    })
+
+
+urlpatterns = [
+    path("", home),
+
+    path("admin/", admin.site.urls),
 
     path(
         "auth/",
         include("accounts.urls")
     ),
 
-     path(
+    path(
         "events/",
         include("events.urls")
     ),
-  path(
-    "api/schema/",
-    SpectacularAPIView.as_view(),
-    name="schema"
-),
 
-path(
-    "api/docs/",
-    SpectacularSwaggerView.as_view(
-        url_name="schema"
+    path(
+        "api/schema/",
+        SpectacularAPIView.as_view(),
+        name="schema"
     ),
-),
-]
 
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(
+            url_name="schema"
+        ),
+    ),
+]
